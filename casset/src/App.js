db6368487b1a6c82e8b5a10cc3620eb442001a30
Gio <gio.devera@ucalgary.ca>
2024-02-15 17:04:06 -0700

    Searching songs works the button does nothing

--- casset/src/App.js
+++ casset/src/App.js
@@ -10,6 +10,8 @@ const CLIENT_SECRET = "d62652ceebc54d32a9292f154adc3e7b";
 function App() {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+  const [songs, setSongs] = useState([]);
 
   useEffect(() => {
     // API Access Token
@@ -32,19 +34,45 @@ function App() {
     console.log("Search for " + searchInput);
 
     // Get request using search to get the Artist ID
-    var artistParams = {
+    var artistSearchParams = {
       method: 'GET',
       headers: {
         'Content-Type' : 'application/json',
         'Authorization' : 'Bearer ' + accessToken
       }
     }
-    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', artistParams)
+    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', artistSearchParams)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {return data.artists.items[0].id})
+
+    console.log("Artist ID is " + artistID);
+
     // like above but using the Artist ID to grab the albums from that artist
 
+    var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=25', artistSearchParams)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setAlbums(data.items);
+      });
     // Display albums
+  }
+
+  async function searchSong(){
+
+    var trackSearchParams = {
+      method: 'GET',
+      headers: {
+        'Content-Type' : 'application/json',
+        'Authorization' : 'Bearer ' + accessToken
+      }
+    }
+    var trackResult = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=track&limit=20', trackSearchParams)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.tracks.items);
+      setSongs(data.tracks.items);
+    })
   }
 
   return (
@@ -56,24 +84,33 @@ function App() {
             type="input"
             onKeyPress={event => {
               if(event.key === "Enter"){
-                search();
+                searchSong();
               }
             }}
             onChange={event => setSearchInput(event.target.value)}
           />
-          <Button onClick={search}>
+          <Button onClick={searchSong}>
             Search
           </Button>
         </InputGroup>
       </Container>
       <Container>
         <Row className="mx-2 row row-cols-4">
-          <Card>
-            <Card.Img src="#"/>
-            <Card.Body>
-              <Card.Title>Album Name Here</Card.Title>
-            </Card.Body>
-          </Card>
+          {songs.map( (song, i) => {
+            console.log(song);
+            return (
+              <Card>
+              <Card.Img src={song.album.images[0].url}/>
+              <Card.Body>
+                <Card.Title>{song.name}</Card.Title>
+                <Card.Subtitle>{song.artists[0].name}</Card.Subtitle>
+              </Card.Body>
+              <Button onClick={() => console.log("Song clicked :)")}>
+                Add to Playlist
+              </Button>
+            </Card>
+            )
+          })}
         </Row>
       </Container>
     </div>
