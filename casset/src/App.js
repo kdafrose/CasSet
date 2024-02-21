e647c918efa1804a4bb48088c6bbde49e76a046d
Gio <gio.devera@ucalgary.ca>
2024-02-21 13:34:01 -0700

    Moved code around, added some routing

--- casset/src/App.js
+++ casset/src/App.js
@@ -1,120 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap'
-import {useState, useEffect} from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SongSearch from './components/SongSearch';
+import Home from './components/Home';
 
 const CLIENT_ID = "836985c6fb334af49ed4a3fb55e973fe";
 const CLIENT_SECRET = "d62652ceebc54d32a9292f154adc3e7b";
 
 function App() {
-  const [searchInput, setSearchInput] = useState("");
-  const [accessToken, setAccessToken] = useState("");
-  const [albums, setAlbums] = useState([]);
-  const [songs, setSongs] = useState([]);
-
-  useEffect(() => {
-    // API Access Token
-    // Make Error Handling????
-    var authorizeParam = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-    }
-    fetch('https://accounts.spotify.com/api/token', authorizeParam)
-    .then(result => result.json())
-    .then(data => setAccessToken(data.access_token))
-  }, [])
-
-  // Search time :)
-
-  async function search(){
-    console.log("Search for " + searchInput);
-
-    // Get request using search to get the Artist ID
-    var artistSearchParams = {
-      method: 'GET',
-      headers: {
-        'Content-Type' : 'application/json',
-        'Authorization' : 'Bearer ' + accessToken
-      }
-    }
-    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', artistSearchParams)
-    .then(response => response.json())
-    .then(data => {return data.artists.items[0].id})
-
-    console.log("Artist ID is " + artistID);
-
-    // like above but using the Artist ID to grab the albums from that artist
-
-    var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=25', artistSearchParams)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setAlbums(data.items);
-      });
-    // Display albums
-  }
-
-  async function searchSong(){
-
-    var trackSearchParams = {
-      method: 'GET',
-      headers: {
-        'Content-Type' : 'application/json',
-        'Authorization' : 'Bearer ' + accessToken
-      }
-    }
-    var trackResult = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=track&limit=20', trackSearchParams)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.tracks.items);
-      setSongs(data.tracks.items);
-    })
-  }
-
   return (
-    <div className="App">
-      <Container>
-        <InputGroup className="mb-3" size="lg">
-          <FormControl
-            placeholder="Search For Artist"
-            type="input"
-            onKeyPress={event => {
-              if(event.key === "Enter"){
-                searchSong();
-              }
-            }}
-            onChange={event => setSearchInput(event.target.value)}
-          />
-          <Button onClick={searchSong}>
-            Search
-          </Button>
-        </InputGroup>
-      </Container>
-      <Container>
-        <Row className="mx-2 row row-cols-4">
-          {songs.map( (song, i) => {
-            console.log(song);
-            return (
-              <Card>
-              <Card.Img src={song.album.images[0].url}/>
-              <Card.Body>
-                <Card.Title>{song.name}</Card.Title>
-                <Card.Subtitle>{song.artists[0].name}</Card.Subtitle>
-              </Card.Body>
-              <Button onClick={() => console.log("Song clicked :)")}>
-                Add to Playlist
-              </Button>
-            </Card>
-            )
-          })}
-        </Row>
-      </Container>
-    </div>
-    );
-  }
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/song" element={<SongSearch/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default App;
