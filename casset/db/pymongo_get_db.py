2c784347c1325638ba252f10de0e5d6afa61971d
JReeseSanchez <jaiden.sanchez@ucalgary.ca>
2024-02-21 13:50:21 -0700

    Starting DB

---
+++ casset/db/pymongo_get_db.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+def get_database():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = "mongodb+srv://user:pass@cluster.mongodb.net/myFirstDatabase"
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['user_shopping_list']
