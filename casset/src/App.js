dd7ff80f2d49031be3e909c35dc504cab4c6a0a0
Gio <gio.devera@ucalgary.ca>
2024-02-21 13:39:34 -0700

    merged with main

--- casset/src/App.js
+++ casset/src/App.js
@@ -5,9 +5,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SongSearch from './components/SongSearch';
 import Home from './components/Home';
 
-const CLIENT_ID = "836985c6fb334af49ed4a3fb55e973fe";
-const CLIENT_SECRET = "d62652ceebc54d32a9292f154adc3e7b";
-
 function App() {
   return (
     <BrowserRouter>
