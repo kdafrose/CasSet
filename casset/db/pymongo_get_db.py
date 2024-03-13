1188faf0ab9d00b141626546a456a8f9de5a118f
JReeseSanchez <jaiden.sanchez@ucalgary.ca>
2024-03-13 15:33:26 -0600

    update

--- casset/db/pymongo_get_db.py
+++ casset/db/pymongo_get_db.py
@@ -1,9 +1,19 @@
 from pymongo import MongoClient
 from connectDB import CONNECTION_STRING
+
 def get_database():
- 
-   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-   client = MongoClient(CONNECTION_STRING)
- 
-   # Create the database for our example (we will use the same database throughout the tutorial
-   return client['user_shopping_list']
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    test = 0
+    
+
+    
+
+if __name__ == "__main__":
+    client = MongoClient(CONNECTION_STRING)
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
