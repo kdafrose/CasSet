d40c1e09825e0239d934b65fa7bcb3142b732da4
Teresa Lavoie <teresa.lavoie@ucalgary.ca>
2024-03-14 16:10:15 -0600

    Bare bones fugly login

--- casset/src/components/Home.js
+++ casset/src/components/Home.js
@@ -4,10 +4,6 @@ export default function Home(){
     return(
         <div style={{height:"90vh"}}>
 
-        <div id = "Nav-bar" style ={{height:"10vh"}}>
-          <h1 style={{}}>CasSet</h1>
-        </div>
-    
         <Googlelogin/>
       </div>
     )
