2a6840ab0d0a6351bf325747acbb64ab9d511f54
batoolhussaini <batool.hussainisyeda@ucalgary.ca>
2024-03-14 18:32:49 -0600

    header + title

--- casset/src/components/Home.js
+++ casset/src/components/Home.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import Googlelogin from './Googlelogin';
+import './Home.css'; // Import CSS file
+import imageSrc from './Header.png'; // Import the image
 
-export default function Home(){
-    return(
-        <div style={{height:"90vh"}}>
-
-        <Googlelogin/>
+export default function Home() {
+  return (
+    <div>
+      <img src={imageSrc} alt="CASSET" className="header" />
+      <div className="page-container"> {/* Use 'page-container' class */}
+        <Googlelogin />
       </div>
-    )
+    </div>
+  );
 }
