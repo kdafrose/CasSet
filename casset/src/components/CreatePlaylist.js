d62c37571fb2693b4b5ba116eb25ff11df4eb94c
JReeseSanchez <jaiden.sanchez@ucalgary.ca>
2024-03-25 13:41:37 -0600

    Fixing Connections

--- casset/src/components/CreatePlaylist.js
+++ casset/src/components/CreatePlaylist.js
@@ -1,8 +1,8 @@
 import {useState} from 'react'
 import {Container, InputGroup, FormControl, Button, Card, Row} from 'react-bootstrap'
 import AddSong from './AddSong';
-import fetchPostMultiSongs from './controller/fetchPostMultiSongs';
-import fetchPostPlaylist from '.controller/fetchPostPlaylist';
+import {fetchPostMultiSongs} from '../controller/fetchPostMultiSongs';
+import {fetchPostPlaylist} from '../controller/fetchPostPlaylist';
 
 // const params = new URLSearchParams(window.location.search);
 // const code = params.get("code");
