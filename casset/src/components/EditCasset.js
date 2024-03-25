4e7d39830c627846da804f90ff10129c21ba71c1
batoolhussaini <batool.hussainisyeda@ucalgary.ca>
2024-03-24 18:13:02 -0600

    just adding song boxes for now will cont later

--- casset/src/components/EditCasset.js
+++ casset/src/components/EditCasset.js
@@ -1,8 +1,10 @@
 import '../css/EditCasset.css';
 import { Button } from 'react-bootstrap';
-import titleSrc from '../media/casset_title_purple.png';
+// hardcoded images here:
 import spotifyCover from '../media/spotifycover.jpg';
 import tempCover from '../media/goatedmusic.png';
+import artistImage from '../media/artistimage.png';
+import artistImage2 from '../media/rina.JPG';
 
 function EditCasset({ onClose }) {
     return (
@@ -10,7 +12,7 @@ function EditCasset({ onClose }) {
             <div id="casset-side-box">
                 <img src={spotifyCover} alt="spotify cover" id="spotify-cover"/>
                 <p className="russo-one-regular" id="spotify-desc-title">description</p>
-                <p id="spotify-desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.</p>
+                <p id="spotify-desc">Lorem ipsum dolor ue s nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.</p>
                 <div id="date-container">
                     <p className="russo-one-regular" id="date-created">date created:</p>
                     <p id="date">Mar 12, 2024</p>
@@ -24,13 +26,52 @@ function EditCasset({ onClose }) {
                     <Button id="back-button" onClick={onClose}>go back</Button>
                     <p className="russo-one-regular" id="casset-songs-title">SONGS</p>
                 </div>
-                <div id="casset-songs-box-col">
+                <div id="casset-songs-box-col" className="scrollable">
                     <div id="casset-songs-row">
                         <img src={tempCover} alt="temp cover" id="casset-cover"/>
                             <p className="russo-one-regular" id="casset-title">goatedmusic.</p>
                     </div>
-                    <div id="casset-list">
-                        {/* TO-DO */}
+                    <div id="casset-list-in-edit">
+                        {/* Song Box HARD CODED FOR NOW*/}
+                        <div className="song-box-info">
+                            <p id="spotify-number" className="russo-one-regular">1</p>
+                            <img src={artistImage} alt="artist image" id="spotify-artist-image"/>
+                            <div>
+                                <p id="spotify-songname-format"><strong>True Romance</strong></p>
+                                <p id="spotify-artistname-format">PinkPantheress</p>
+                                {/* <p id="spotify-albumname-format">Heaven Knows</p> */}
+                            </div>
+                            
+                        </div>
+
+                        <div className="song-box-info">
+                        <p id="spotify-number" className="russo-one-regular">2</p>
+                            <img src={artistImage2} alt="artist image" id="spotify-artist-image"/>
+                            <div>
+                                <p id="spotify-songname-format"><strong>Cyber Stockholm Syndrome</strong></p>
+                                <p id="spotify-artistname-format">Rina Sawayama</p>
+                                {/* <p id="spotify-albumname-format">Heaven Knows</p> */}
+                            </div>
+                            
+                        </div>
+                        <div className="song-box-info">
+                            <p>Song 3</p>
+                            
+                        </div>
+                        <div className="song-box-info">
+                            <p>Song 3</p>
+                            
+                        </div>
+                        <div className="song-box-info">
+                            <p>Song 3</p>
+                            
+                        </div>
+                        <div className="song-box-info">
+                            <p>Song 3</p>
+                            
+                        </div>
+                        
+                        
                     </div>
                 </div>
             </div>
