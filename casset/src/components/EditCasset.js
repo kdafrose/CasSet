f743ae788c0ce43f015e97b9168bab622ac4ab74
kdafrose <katherine.tubang@ucalgary.ca>
2024-03-25 22:21:01 -0600

    Edit casset songs stuff shows :DD

--- casset/src/components/EditCasset.js
+++ casset/src/components/EditCasset.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/EditCasset.css';
 import { Button } from 'react-bootstrap';
 import Note from './Note'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; //senorita awesome!
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons'; // Import the trash icon
+
+// Importing songs from db
+import fetchGetMultiSongs from '../controller/fetchMultiSongs';
+import fetchCasset from '../controller/fetchSinglePlaylist';
 
 // Hardcoded images here
 import spotifyCover from '../media/spotifycover.jpg';
@@ -11,8 +15,11 @@ import tempCover from '../media/goatedmusic.png';
 import artistImage from '../media/artistimage.png';
 import artistImage2 from '../media/rina.JPG';
 
-function EditCasset({ onClose }) {
+function EditCasset({ onClose, playlistID }) {
     // Sample song data
+
+    const [songsDocs, setSongsDocs] = useState([]);
+    const [selectedPlaylist, setSelectedPlaylist] = useState([]);
     const songs = [
         {
             title: "True Romance",
@@ -25,7 +32,37 @@ function EditCasset({ onClose }) {
             cover: artistImage2
         },
     ];
+
+    // gets songs information
+    useEffect(() => {
+        const fetchSongsDocs = async () => {
+            try {
+                const songsItems = await fetchGetMultiSongs(playlistID);
+                setSongsDocs(songsItems);
+                console.log(songsDocs);
+            } catch (error) {
+                console.error('Error fetching songs:', error);
+            }
+        };
     
+        fetchSongsDocs();
+    }, [playlistID]); // Make sure to include playlistID in the dependency array
+    
+    // gets playlist information
+    useEffect(() => {
+        const fetchSelectedPlaylist = async () => {
+            try {
+                const chosenPlaylist = await fetchCasset(playlistID);
+                setSelectedPlaylist(chosenPlaylist);
+                console.log(selectedPlaylist);
+            } catch (error) {
+                console.error('Error fetching playlist:', error);
+            }
+        }
+
+        fetchSelectedPlaylist();
+    }, [playlistID]);
+
     // Functionality for delete button (for now, same as back button)
     const handleDelete = () => {
         const isConfirmed = window.confirm('Are you sure you want to delete this casset?');
@@ -39,10 +76,10 @@ function EditCasset({ onClose }) {
             <div id="casset-side-box">
                 <img src={spotifyCover} alt="spotify cover" id="spotify-cover"/>
                 <p className="russo-one-regular" id="spotify-desc-title">description</p>
-                <p id="spotify-desc">Lorem ipsum dolor ue s nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.</p>
+                <p id="spotify-desc">{selectedPlaylist.note}</p>
                 <div id="date-container">
                     <p className="russo-one-regular" id="date-created">date created:</p>
-                    <p id="date">Mar 12, 2024</p>
+                    <p id="date">{selectedPlaylist.date_created}</p>
                 </div>
                 <div id="share-button-div">
                     <button type="button" className="russo-one-regular" id="share-button">share</button>
@@ -58,16 +95,16 @@ function EditCasset({ onClose }) {
                 <div id="casset-songs-box-col" className="scrollable">
                     <div id="casset-songs-row">
                         <img src={tempCover} alt="temp cover" id="casset-cover"/>
-                            <p className="russo-one-regular" id="casset-title">goatedmusic.</p>
+                            <p className="russo-one-regular" id="casset-title">{selectedPlaylist.playlist_name}</p>
                     </div>
                     <div id="casset-list-in-edit">
-                        {songs.map((song, index) => (
+                        {songsDocs.map((song, index) => (
                             <div>
                                 <div key={`song-${index}`} className="song-box-info">
                                     <p id="spotify-number" className="russo-one-regular">{index + 1}</p>
-                                    <img src={song.cover} alt="artist image" id="spotify-artist-image"/>
+                                    <img src={song.song_image} alt="artist image" id="spotify-artist-image"/>
                                     <div>
-                                        <p id="spotify-songname-format"><strong>{song.title}</strong></p>
+                                        <p id="spotify-songname-format"><strong>{song.name}</strong></p>
                                         <p id="spotify-artistname-format">{song.artist}</p>
                                     </div>
                                 </div>
