e8d800a17743e965c172ffb6a43d67f07532438a
Teresa Lavoie <teresa.lavoie@ucalgary.ca>
2024-03-27 12:24:37 -0600

    close share popup after adding friend

--- casset/src/components/EditCasset.js
+++ casset/src/components/EditCasset.js
@@ -95,6 +95,8 @@ function EditCasset({ onClose, playlistID, friends }    ) {
         if (isConfirmed) {
             console.log(`Sharing playlist ${selectedPlaylist.playlist_name} with friend:`, friend);
             // Logic to share the playlist with the selected friend...
+
+            setShowSharePopup(false);
         }
     };
 
