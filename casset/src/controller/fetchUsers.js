8ef9383d72f314e88dc4cff925b48c3bd4e2bbe6
kdafrose <katherine.tubang@ucalgary.ca>
2024-03-27 01:19:40 -0600

    fixed fetch URL

--- casset/src/controller/fetchUsers.js
+++ casset/src/controller/fetchUsers.js
@@ -2,20 +2,20 @@ const fetchUsers = async () => {
     try {
         const profileInfo = JSON.parse(localStorage.getItem('profile'));
         console.log(profileInfo);
-        const response = await fetch('http://localhost:5000/users/findUsers', {
+        const response = await fetch('http://localhost:5000/users/searchUsers', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ "userID": data['userID'] }), 
+            body: JSON.stringify({ "email": profileInfo['email'] }), 
         });
 
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
 
-        const data = await response.json();
-        return data;
+        const foundUser = await response.json();
+        return foundUser;
     } catch (error) {
         console.log(error);
     }
